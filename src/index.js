4efca06e2ea3f3ef968b67b6ac3cd927d8ef6e48
Barbara Mayfield <bmayfield1991@gmail.com>
2020-03-17 09:39:08 -0500

    Initial setup of Redux to work with Apollo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+
+// Redux & Apollo
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { cache }  from './utils/cache';
 
 // styles
 import './index.css';
@@ -9,13 +17,27 @@ import './index.css';
 // components
 import App from './components/App';
 
-const AppWithRouter = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+    link: new HttpLink({
+      uri: 'http://localhost:3000/graphql' //heroku
+    }),
+    connectToDevTools: true,
+    cache,
+})
+
+const AppWithProvider = (
+<ApolloProvider client={client}>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+</ApolloProvider>
 )
 
-ReactDOM.render(AppWithRouter, document.getElementById('root'));
+ReactDOM.render(AppWithProvider, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
