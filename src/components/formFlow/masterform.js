9468fe3f5c55256fba40d4e709dcb7f8ecb10bbe
Chase Wallace <wallacecs007@gmail.com>
2020-05-11 17:39:12 -0600

    Committing to pull

--- src/components/formFlow/masterform.js
+++ src/components/formFlow/masterform.js
@@ -2,34 +2,32 @@ import React, { useState } from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 
 //Components import
-import GeneralInfo from './generalInfo.js'
-import Education from './education.js'
-import WorkHistory from "./workHistory"
-import EndFormReview from "./reviewForm/endFormReview"
-import PersonalProjects from './PersonalProjects'
-import TechSkills from './TechSkills.js'
+import GeneralInfo from "./generalInfo.js";
+import Education from "./education.js";
+import WorkHistory from "./workHistory";
+import EndFormReview from "./reviewForm/endFormReview";
+import PersonalProjects from "./PersonalProjects";
+import TechSkills from "./TechSkills.js";
 
 //State import
 // import
 
 function MasterForm() {
+  const { path, url } = useRouteMatch();
 
-    const {path, url} = useRouteMatch();
-
-    return(
-        <div>
-            <h1>Resume Form</h1>
-            <Switch>
-                <Route path={`${url}/generalInfo`} component={GeneralInfo} />
-                <Route path={`${url}/education`} component ={Education}/>
-                <Route path={`${url}/work`} component={WorkHistory} />
-                <Route path={`${url}/review`} component={EndFormReview}/> 
-                <Route path={`${url}/projects`} component={PersonalProjects} />
-                <Route path={`${url}/techskills`} component={TechSkills}/>
-            </Switch>
-        </div>
-    )
-
+  return (
+    <div>
+      <h1>Resume Form</h1>
+      <Switch>
+        <Route path={`${url}/generalInfo`} component={GeneralInfo} />
+        <Route path={`${url}/education`} component={Education} />
+        <Route path={`${url}/work`} component={WorkHistory} />
+        <Route path={`${url}/review`} component={EndFormReview} />
+        <Route path={`${url}/projects`} component={PersonalProjects} />
+        <Route path={`${url}/techskills`} component={TechSkills} />
+      </Switch>
+    </div>
+  );
 }
 
 export default MasterForm;
