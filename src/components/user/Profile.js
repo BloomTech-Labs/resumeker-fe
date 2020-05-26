44bab81094ace03f5d31b727fb43ec376f0ea687
Vyraal1 <kevin.afable@gmail.com>
2020-05-25 20:58:11 -0400

    fix: draft conditional rendering

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -28,47 +28,44 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Profile(props) {
+    console.log(props);
     const { loading, error, data } = useQuery(GET_DRAFTS_QUERY);
     const classes = useStyles();
     console.log("We are in the profile");
 
     if (loading) return <div>Loading</div>;
-    if (error) return <div>{error}</div>;
+    if (error) return <div>{"error, check console"}</div>;
+
     const { getDrafts } = data;
     console.log(getDrafts, "profile query response");
 
-    if (data) {
-        if (data.getDrafts.length) {
-            console.log("Drafts #2");
-            return data.getDrafts.map((draft) => (
-                <ResumeCard key={draft.id} id={draft.id} />
-            ));
-        } else {
-            console.log("No drafts");
-            return <div>No Drafts</div>;
-        }
-    } else if (loading) {
+    if (loading) {
         console.log("It's loading");
         return <div>Loading...</div>;
     }
 
-    return (
-        <div className="profile">
-            <div className={classes.container}>
-                <div
-                    className="resume-container"
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    {getDrafts.map((draft) => (
-                        <ResumeCard key={draft.id} draftID={draft.id} />
-                    ))}
-                </div>
+    if (data) {
+        return (
+            <div className="profile">
+                <div className={classes.container}>
+                    <div
+                        className="resume-container"
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                        }}
+                    >
+                        {data.getDrafts.length > 0 ? (
+                            getDrafts.map((draft) => (
+                                <ResumeCard key={draft.id} draftID={draft.id} />
+                            ))
+                        ) : (
+                            <div>No Drafts</div>
+                        )}
+                    </div>
 
-                {/* <Button
+                    {/* <Button
           variant="contained"
           color="primary"
           className={classes.addResume}
@@ -79,20 +76,21 @@ export default function Profile(props) {
         >
           Add a New Resume
         </Button> */}
-            </div>
+                </div>
 
-            <div className={classes.infoContainer}>
-                <UserData />
-                <NavLink to="/form/generalInfo">
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        className={classes.addResume}
-                    >
-                        Add a New Resume
-                    </Button>
-                </NavLink>
+                <div className={classes.infoContainer}>
+                    <UserData />
+                    <NavLink to="/form/generalInfo">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.addResume}
+                        >
+                            Add a New Resume
+                        </Button>
+                    </NavLink>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
