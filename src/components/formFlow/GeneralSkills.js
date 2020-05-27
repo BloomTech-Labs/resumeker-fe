1ad35c4adf34807a6bd4489b9e1e52c3e624c846
Chase Wallace <wallacecs007@gmail.com>
2020-05-26 18:15:02 -0600

    Committing to Pull

--- src/components/formFlow/GeneralSkills.js
+++ src/components/formFlow/GeneralSkills.js
@@ -10,24 +10,24 @@ import { DRAFT_ID } from "../../queries/draftID";
 
 //Actions
 import {
-    addGeneralSkill,
-    removeGeneralSkill,
+  addGeneralSkill,
+  removeGeneralSkill,
 } from "../../actions/resumeFormActions.js";
 
 import SingleFieldFormTemplate from "./formsTemplate/singleFieldFormTemplate";
 import TipsLayout from "./formUtils/tipsLayout";
 
 import {
-    Button,
-    CssBaseline,
-    Paper,
-    Grid,
-    Typography,
-    makeStyles,
-    Chip,
+  Button,
+  CssBaseline,
+  Paper,
+  Grid,
+  Typography,
+  makeStyles,
+  Chip,
 } from "@material-ui/core";
 
-import MobileStepper from '@material-ui/core/MobileStepper';
+import MobileStepper from "@material-ui/core/MobileStepper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -84,223 +84,207 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     display: "flex",
     justifyContent: "center",
-},
+  },
 }));
 
 function GeneralSkills(props) {
-    const { data } = useQuery(DRAFT_ID);
+  const { data } = useQuery(DRAFT_ID);
 
-    const [info, setInfo] = useState({
-        draftID: "",
-        skill: "",
-    });
+  const [info, setInfo] = useState({
+    draftID: "",
+    skill: "",
+  });
 
-    const [activeStep, setActiveStep] = useState(6);
+  const [activeStep, setActiveStep] = useState(6);
 
-    //Instantiate useMutation Hook / Creates tuple with 1st var being actual
-    //call function, and 2nd destructured variable being return data and tracking
-    const [addSkill, { loading, error }] = useMutation(ADD_SKILL_MUTATION, {
-        onCompleted(data) {
-            console.log(data, "\n Add General Skill Response");
+  //Instantiate useMutation Hook / Creates tuple with 1st var being actual
+  //call function, and 2nd destructured variable being return data and tracking
+  const [addSkill, { loading, error }] = useMutation(ADD_SKILL_MUTATION, {
+    onCompleted(data) {
+      console.log(data, "\n Add General Skill Response");
+    },
+  });
+
+  const classes = useStyles();
+
+  const nextPage = (event) => {
+    event.preventDefault();
+    if (info.skill.length > 0) {
+      props.addGeneralSkill(info);
+
+      //Apollo useMutation API call to send data to backend
+      addSkill({
+        variables: {
+          input: {
+            draftID: localStorage.getItem("draftID"),
+            skillType: "Qualitative",
+            name: info.skill,
+          },
         },
+      });
+    }
+    props.setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    props.history.push("/form/languages");
+  };
+
+  const anotherSkill = (event) => {
+    event.preventDefault();
+    if (info.skill.length > 0) {
+      props.addGeneralSkill(info);
+
+      //Apollo useMutation API call to send data to backend
+      addSkill({
+        variables: {
+          input: {
+            draftID: localStorage.getItem("draftID"),
+            skillType: "Qualitative",
+            name: info.skill,
+          },
+        },
+      });
+    }
+
+    setInfo({
+      ...info,
+      skill: "",
     });
+  };
+  const onChange = (event) => {
+    event.preventDefault();
+    setInfo({ ...info, [event.target.name]: event.target.value });
+  };
 
-    const classes = useStyles();
+  const handleDelete = (skillToDelete) => (event) => {
+    event.preventDefault();
+    props.removeGeneralSkill(skillToDelete);
+    setInfo({ ...info });
+  };
 
-    const nextPage = (event) => {
-        event.preventDefault()
-        if (info.skill.length > 0) {
-            props.addGeneralSkill(info);
-
-            //Apollo useMutation API call to send data to backend
-            addSkill({
-                variables: {
-                    input: {
-                        draftID: localStorage.getItem("draftID"),
-                        skillType: "Qualitative",
-                        name: info.skill,
-                    }
-                },
-            });
-        }
-        props.setActiveStep((prevActiveStep) => prevActiveStep + 1)
-        props.history.push("/form/languages");
-    };
-
-    const anotherSkill = (event) => {
-        event.preventDefault();
-        if (info.skill.length > 0) {
-            props.addGeneralSkill(info);
-
-            //Apollo useMutation API call to send data to backend
-            addSkill({
-                variables: {
-                    input: {
-                        draftID: localStorage.getItem("draftID"),
-                        skillType: "Qualitative",
-                        name: info.skill,
-                    }
-                },
-            });
-        }
-
-        setInfo({
-            ...info,
-            skill: "",
-        });
-    };
-    const onChange = (event) => {
-        event.preventDefault();
-        setInfo({ ...info, [event.target.name]: event.target.value });
-    };
-
-    const handleDelete = (skillToDelete) => (event) => {
-        event.preventDefault();
-        props.removeGeneralSkill(skillToDelete);
-        setInfo({ ...info });
-    };
-
-    return (
-        <div>
-            <Grid container componet="main" className={classes.root}>
-                <CssBaseline />
-                <TipsLayout tips={Tip()} />
-                <Grid
-                    item
-                    xs={12}
-                    sm={8}
-                    md={9}
-                    component={Paper}
-                    elevation={6}
-                    square
+  return (
+    <div>
+      <Grid container componet="main" className={classes.root}>
+        <CssBaseline />
+        <TipsLayout tips={Tip()} />
+        <Grid item xs={12} sm={8} md={9} component={Paper} elevation={6} square>
+          <MobileStepper
+            variant="progress"
+            steps={8}
+            position="static"
+            activeStep={props.activeStep}
+            className={classes.progress}
+          />
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
+              Tell us about some of your other skills that didn't quite make it
+              into the technical section!
+            </Typography>
+            <Typography
+              color="textSecondary"
+              component="h5"
+              variant="subtitle2"
+            >
+              (Time Management, Critical Thinking, Teamwork, Problem Solving,
+              Comunication, etc)
+            </Typography>
+            <form
+              className={classes.form}
+              onSubmit={anotherSkill}
+              id="generalSkillsForm"
+            >
+              <SingleFieldFormTemplate
+                onChange={onChange}
+                info={info.skill}
+                anotherOne={(e) => anotherSkill(e)}
+                name="skill"
+                label="General Skill"
+              />
+              <Grid className={classes.skillContainer}>
+                <Paper
+                  component="ul"
+                  square="true"
+                  className={classes.chipContainer}
                 >
-                    <MobileStepper
-                    variant="progress"
-                    steps={8}
-                    position="static"
-                    activeStep={props.activeStep}
-                    className={classes.progress}
-                    />
-                    <div className={classes.paper}>
-                        <Typography component="h1" variant="h5">
-                            Tell us about some of your other skills that didn't
-                            quite make it into the technical section!
-                        </Typography>
-                        <Typography
-                            color="textSecondary"
-                            component="h5"
-                            variant="subtitle2"
-                        >
-                            (Time Management, Critical Thinking, Teamwork,
-                            Problem Solving, Comunication, etc)
-                        </Typography>
-                        <form
-                            className={classes.form}
-                            onSubmit={anotherSkill}
-                            id="generalSkillsForm"
-                        >
-                            <SingleFieldFormTemplate
-                                onChange={onChange}
-                                info={info.skill}
-                                anotherOne={(e) => anotherSkill(e)}
-                                name="skill"
-                                label="General Skill"
-                            />
-                            <Grid className={classes.skillContainer}>
-                                <Paper
-                                    component="ul"
-                                    square="true"
-                                    className={classes.chipContainer}
-                                >
-                                    <Chip
-                                        label="Your Skills:"
-                                        className={classes.chip}
-                                        color="primary"
-                                    />
-                                    {props.resumeData.generalSkills.map(
-                                        (data) => {
-                                            return (
-                                                <li
-                                                    key={data.id}
-                                                    className="listOfGeneralSkills"
-                                                >
-                                                    <Chip
-                                                        label={data.skill}
-                                                        onDelete={handleDelete(
-                                                            data
-                                                        )}
-                                                        className={classes.chip}
-                                                    />
-                                                </li>
-                                            );
-                                        }
-                                    )}
-                                </Paper>
-                            </Grid>
+                  <Chip
+                    label="Your Skills:"
+                    className={classes.chip}
+                    color="primary"
+                  />
+                  {props.resumeData.generalSkills.map((data) => {
+                    return (
+                      <li key={data.id} className="listOfGeneralSkills">
+                        <Chip
+                          label={data.skill}
+                          onDelete={handleDelete(data)}
+                          className={classes.chip}
+                        />
+                      </li>
+                    );
+                  })}
+                </Paper>
+              </Grid>
 
-                            <Grid className={classes.buttonContainer}>
-                                <Button
-                                    type="button"
-                                    fullWidth
-                                    variant="outlined"
-                                    color="primary"
-                                    id="previous_techSkills"
-                                    className={`${classes.previousButton} singlePageButton`}
-                                    onClick={() => {
-                                        props.setActiveStep((prevActiveStep) => prevActiveStep - 1)
-                                        props.history.push("/form/techskills");
-                                    }}
-                                >
-                                    Previous Form
-                                </Button>
-                                <Button
-                                    type="button"
-                                    fullWidth
-                                    variant="contained"
-                                    color="primary"
-                                    id="next_languages"
-                                    className={`${classes.nextButton} singlePageButton`}
-                                    onClick={(e)=>nextPage(e)}
-                                >
-                                    Next Form
-                                </Button>
-                            </Grid>
-                        </form>
-                    </div>
-                </Grid>
-            </Grid>
-            <button onClick={() => nextPage()}>Next Page</button>
-        </div>
-    );
+              <Grid className={classes.buttonContainer}>
+                <Button
+                  type="button"
+                  fullWidth
+                  variant="outlined"
+                  color="primary"
+                  id="previous_techSkills"
+                  className={`${classes.previousButton} singlePageButton`}
+                  onClick={() => {
+                    props.setActiveStep((prevActiveStep) => prevActiveStep - 1);
+                    props.history.push("/form/techskills");
+                  }}
+                >
+                  Previous Form
+                </Button>
+                <Button
+                  type="button"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  id="next_languages"
+                  className={`${classes.nextButton} singlePageButton`}
+                  onClick={(e) => nextPage(e)}
+                >
+                  Next Form
+                </Button>
+              </Grid>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+      <button onClick={() => nextPage()}>Next Page</button>
+    </div>
+  );
 }
 
 function Tip() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-            <p className={classes.tipTextLarge}>
-                Employers look for a variety of strong soft skills as it helps
-                ensure a productive, collaborative, and healthy work
-                environment!
-            </p>
-            <p className={classes.tipTextSmall}>
-                Do your best to insure these skills are action skills! (Ex.
-                Organized, Communicated, Negotiated, Adapted)
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p className={classes.tipTextLarge}>
+        Employers look for a variety of strong soft skills as it helps ensure a
+        productive, collaborative, and healthy work environment!
+      </p>
+      <p className={classes.tipTextSmall}>
+        Do your best to insure these skills are action skills! (Ex. Organized,
+        Communicated, Negotiated, Adapted)
+      </p>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        resumeData: state.resumeFormReducer.resumeData,
-        resumeError: state.resumeFormReducer.error,
-        resumeLoading: state.resumeFormReducer.loading,
-    };
+  return {
+    resumeData: state.resumeFormReducer.resumeData,
+    resumeError: state.resumeFormReducer.error,
+    resumeLoading: state.resumeFormReducer.loading,
+  };
 };
 
 export default connect(mapStateToProps, {
-    addGeneralSkill,
-    removeGeneralSkill,
+  addGeneralSkill,
+  removeGeneralSkill,
 })(GeneralSkills);
--- src/components/formFlow/generalInfo.js
+++ src/components/formFlow/generalInfo.js
@@ -9,186 +9,169 @@ import GeneralInfoFormTemplate from "./formsTemplate/generalInfoFormTemplate";
 import TipsLayout from "./formUtils/tipsLayout";
 
 import {
-    Button,
-    CssBaseline,
-    Paper,
-    Grid,
-    makeStyles,
+  Button,
+  CssBaseline,
+  Paper,
+  Grid,
+  makeStyles,
 } from "@material-ui/core";
 
 import MobileStepper from "@material-ui/core/MobileStepper";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        height: "100vh",
-    },
-    paper: {
-        margin: theme.spacing(8, 4),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    form: {
-        width: "100%", // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-    tipTextLarge: {
-        fontSize: "1.1rem",
-    },
-    tipTextSmall: {
-        fontSize: "0.8rem",
-    },
-    bold: {
-        fontWeight: "900",
-    },
-    progress: {
-        width: "100%",
-        flexGrow: 1,
-        display: "flex",
-        justifyContent: "center",
-    },
+  root: {
+    height: "100vh",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  tipTextLarge: {
+    fontSize: "1.1rem",
+  },
+  tipTextSmall: {
+    fontSize: "0.8rem",
+  },
+  bold: {
+    fontWeight: "900",
+  },
+  progress: {
+    width: "100%",
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+  },
 }));
 
 export default function GeneralInfo(props) {
-    const [info, setInfo] = useState({
-        email: "",
-        firstName: "",
-        lastName: "",
-    });
+  const [info, setInfo] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+  });
 
-    //Instantiate useMutation Hook / Creates tuple with 1st var being actual
-    //call function, and 2nd destructured variable being return data and tracking
-    const [addDraft, { loading, error, data }] = useMutation(
-        ADD_DRAFT_MUTATION,
-        {
-            onCompleted(data) {
-                // write data to the Apollo store here
-                // https://www.apollographql.com/docs/react/data/local-state/
-                console.log(data, "\n Add Education Response");
+  //Instantiate useMutation Hook / Creates tuple with 1st var being actual
+  //call function, and 2nd destructured variable being return data and tracking
+  const [addDraft, { loading, error, data }] = useMutation(ADD_DRAFT_MUTATION, {
+    onCompleted(data) {
+      // write data to the Apollo store here
+      // https://www.apollographql.com/docs/react/data/local-state/
+      console.log(data, "\n Add Education Response");
 
-                localStorage.setItem("draftID", data.addDraft);
-            },
-        }
-    );
+      localStorage.setItem("draftID", data.addDraft);
+    },
+  });
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const nextPage = (event) => {
-        event.preventDefault();
-        // props.addData(info);
-        console.log("NextPage inside of GeneralInfo");
-        const name = info.firstName + " " + info.lastName;
+  const nextPage = (event) => {
+    event.preventDefault();
+    // props.addData(info);
+    console.log("NextPage inside of GeneralInfo");
+    const name = info.firstName + " " + info.lastName;
 
-        //Calls addDraft Mutation only if component state
-        //is NOT empty
-        if (
-            info.firstName.length > 0 &&
-            info.lastName.length > 0 &&
-            info.email.length > 0
-        ) {
-            //Apollo useMutation API call to send data to backend
-            addDraft({
-                variables: {
-                    input: {
-                        name: name,
-                        email: info.email,
-                    },
-                },
-            });
-        }
+    //Calls addDraft Mutation only if component state
+    //is NOT empty
+    if (
+      info.firstName.length > 0 &&
+      info.lastName.length > 0 &&
+      info.email.length > 0
+    ) {
+      //Apollo useMutation API call to send data to backend
+      addDraft({
+        variables: {
+          input: {
+            name: name,
+            email: info.email,
+          },
+        },
+      });
+    }
 
-        //Resets Component State to ''
-        // setInfo({
-        //     email: "",
-        //     firstName: "",
-        //     lastName: "",
-        // });
-        props.setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    //Resets Component State to ''
+    // setInfo({
+    //     email: "",
+    //     firstName: "",
+    //     lastName: "",
+    // });
+    props.setActiveStep((prevActiveStep) => prevActiveStep + 1);
 
-        props.history.push("/form/education");
-    };
+    props.history.push("/form/education");
+  };
 
-    const onChange = (event) => {
-        console.log(event.target.name, "onChange");
-        setInfo({ ...info, [event.target.name]: event.target.value });
-    };
+  const onChange = (event) => {
+    console.log(event.target.name, "onChange");
+    setInfo({ ...info, [event.target.name]: event.target.value });
+  };
 
-    if (loading) return <div>loading : {loading}</div>;
-    if (error) return <div>{error}</div>;
+  if (loading) return <div>loading : {loading}</div>;
+  if (error) return <div>{error}</div>;
 
-    console.log("outside of onChange", data);
+  console.log("outside of onChange", data);
 
-    return (
-        <div id="generalInfoForm">
-            <Grid container componet="main" className={classes.root}>
-                <CssBaseline />
-                <TipsLayout tips={Tip()} />
-                <Grid
-                    item
-                    xs={12}
-                    sm={8}
-                    md={9}
-                    component={Paper}
-                    elevation={6}
-                    square
-                >
-                    <MobileStepper
-                        variant="progress"
-                        steps={8}
-                        position="static"
-                        activeStep={props.activeStep}
-                        className={classes.progress}
-                    />
-                    <div className={classes.paper}>
-                        <form className={classes.form}>
-                            <GeneralInfoFormTemplate
-                                onChange={onChange}
-                                info={info}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                onClick={nextPage}
-                                className={classes.submit}
-                            >
-                                Next
-                            </Button>
-                        </form>
-                    </div>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  return (
+    <div id="generalInfoForm">
+      <Grid container componet="main" className={classes.root}>
+        <CssBaseline />
+        <TipsLayout tips={Tip()} />
+        <Grid item xs={12} sm={8} md={9} component={Paper} elevation={6} square>
+          <MobileStepper
+            variant="progress"
+            steps={8}
+            position="static"
+            activeStep={props.activeStep}
+            className={classes.progress}
+          />
+          <div className={classes.paper}>
+            <form className={classes.form}>
+              <GeneralInfoFormTemplate onChange={onChange} info={info} />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={nextPage}
+                className={classes.submit}
+              >
+                Next
+              </Button>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 //tips="Hey, welcome to Resumeker forms! These forms will help you create a beautiful
 //resume to impress your potential employers!"
 
 function Tip() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-            <p className={classes.tipTextLarge}>
-                Hey, Welcome to Resumeker Forms!
-            </p>
-            <p className={classes.tipTextLarge}>
-                This Form Flow process will help you create a beautiful and
-                effective resume for potential employers.
-            </p>
-            <p className={classes.tipTextSmall}>
-                Keep an eye out for these{" "}
-                <span className={classes.bold}>tips</span> as they will help you
-                along the way!
-            </p>
-        </div>
-    );
+  return (
+    <div>
+      <p className={classes.tipTextLarge}>Hey, Welcome to Resumeker Forms!</p>
+      <p className={classes.tipTextLarge}>
+        This Form Flow process will help you create a beautiful and effective
+        resume for potential employers.
+      </p>
+      <p className={classes.tipTextSmall}>
+        Keep an eye out for these <span className={classes.bold}>tips</span> as
+        they will help you along the way!
+      </p>
+    </div>
+  );
 }
